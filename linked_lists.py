8fa152edd53729c3af3f9c06583c3f71fcfae9f6
Santiago Caraballo <48489427+sancara@users.noreply.github.com>
2023-02-14 17:18:37 -0300

    linked_lists.py: create stringify_list() method

--- linked_lists.py
+++ linked_lists.py
@@ -1,7 +1,15 @@
 from . import nodes.Node
 
 class LinkedList:
-
+    '''
+    Linked Lists are a sequence of nodes.
+    It starts with a head, and can be uni o bidirectional,
+    - get_head_node returns where the linked list starts.
+    - insert_beginning first instanciate a new node and then links it to the current head_node,
+    consecuently after setting the new node as head node it becomes the first node in the linked list
+    without losing any information.
+    - stringify_list shows the values from each node in the linked list
+    '''
     def __init__(self, value=None):
         self.head_node = Node(value)
 
@@ -14,3 +22,12 @@ class LinkedList:
         new_node.set_next_node(self.head_node)
         self.head_node = new_node
 
+    def stringify_list(self):
+
+        string_list = ""
+        current_node = self.get_head_node()
+        while current_node:
+            if current_node.get_value() != None:
+                string_list += f'{current_node.get_value()}
+            current_node = current_node.get_next_node()
+        return string_list
