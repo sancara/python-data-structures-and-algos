cab4d90f18e59d889452827bd80cc8937b03114a
Santiago Caraballo <48489427+sancara@users.noreply.github.com>
2023-02-14 17:54:50 -0300

    linked_lists.py: remove_node() method added

--- linked_lists.py
+++ linked_lists.py
@@ -9,6 +9,7 @@ class LinkedList:
     consecuently after setting the new node as head node it becomes the first node in the linked list
     without losing any information.
     - stringify_list shows the values from each node in the linked list
+    - remove_node remove a node by it's value
     '''
     def __init__(self, value=None):
         self.head_node = Node(value)
@@ -17,13 +18,11 @@ class LinkedList:
         return self.head_node
 
     def insert_beginning(self, new_value):
-
         new_node = Node(new_value)
         new_node.set_next_node(self.head_node)
         self.head_node = new_node
 
     def stringify_list(self):
-
         string_list = ""
         current_node = self.get_head_node()
         while current_node:
@@ -31,3 +30,17 @@ class LinkedList:
                 string_list += f'{current_node.get_value()}
             current_node = current_node.get_next_node()
         return string_list
+    
+    def remove_node(self, value_to_remove):
+        current_node = self.head_node
+    
+        if current_node.get_value() == value_to_remove:
+            self.head_node = current_node.get_next_node()
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()  
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node
