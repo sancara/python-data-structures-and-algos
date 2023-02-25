8d9838cbeb90903a93c6ee5a03865d7d59857cc0
Santiago Caraballo <48489427+sancara@users.noreply.github.com>
2023-02-24 21:27:17 -0300

    'linked_lists.py: find_middle function declared, two pointers different speeds'

--- linked_lists.py
+++ linked_lists.py
@@ -116,7 +116,6 @@ def nth_last_node(linked_list, n):
 
   from LinkedList import LinkedList
 
-# Complete this function:
 def find_middle(linked_list):
   fast = linked_list.head_node
   slow = linked_list.head_node
@@ -126,15 +125,3 @@ def find_middle(linked_list):
       fast = fast.get_next_node()
       slow = slow.get_next_node()
   return slow
-
-def generate_test_linked_list(length):
-  linked_list = LinkedList()
-  for i in range(length, 0, -1):
-    linked_list.insert_beginning(i)
-  return linked_list
-
-# Use this to test your code:
-test_list = generate_test_linked_list(7)
-print(test_list.stringify_list())
-middle_node = find_middle(test_list)
-print(middle_node.value)
