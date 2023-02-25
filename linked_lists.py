24c878ae94fa8d191121038030017e4ba72523a8
Santiago Caraballo <48489427+sancara@users.noreply.github.com>
2023-02-24 21:14:33 -0300

    'linked_lists.py: two pointers moving in parallel for getting 2nd last child'

--- linked_lists.py
+++ linked_lists.py
@@ -92,10 +92,20 @@ def swap_nodes(input_list, val1, val2):
   node2.set_next_node(temp)
 
 
-ll = LinkedList.LinkedList()
-for i in range(10):
-  ll.insert_beginning(i)
+'''
+Two Pointer Moving in parallel
+'''
 
-print(ll.stringify_list())
-swap_nodes(ll, 9, 5)
-print(ll.stringify_list())
+def nth_last_node(linked_list, n):
+  current = None
+  tail_seeker = linked_list.head_node
+  count = 1
+  while tail_seeker:
+    tail_seeker = tail_seeker.get_next_node()
+    count += 1
+    if count >= n + 1:
+      if current is None:
+        current = linked_list.head_node
+      else:
+        current = current.get_next_node()
+  return current
